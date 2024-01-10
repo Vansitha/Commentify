30445e168a38260a4f1a15b165fad6c94a93a68d
Vansitha Ratnayake <vansitha1@gmail.com>
2024-01-10 13:26:38 +0530

    chore: setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  colors: {},
+  theme: {
+    fontFamily: {
+      title: ["Inconsolata"],
+      body: ["Poppins"],
+    },
+  },
+  plugins: [],
+};
