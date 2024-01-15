565ef98e165a2ebf6d51d7ad77b6bd46f2c2d77f
Vansitha Ratnayake <vansitha1@gmail.com>
2024-01-15 12:20:02 +0530

    chore: add router

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
     <div className='h-screen flex flex-col jusitfy-between'>
       <div className='container mx-auto font-body'>
         {/* Header Component start */}
-        <div className='flex justify-between pt-5'>
+        <div className='flex justify-between pt-5 px-7'>
           <div className='flex items-center'>
-            <h1 className='font-title font-bold text-2xl cursor-pointer'>
+            <h1 className='font-title font-bold text-xl cursor-pointer'>
               &lt;Commentify/&gt;
             </h1>
-            <span className='ms-2 bg-sky-200 text-blue-600 text-xs w-10 h-5 px-10 py-3 flex items-center justify-center font-medium rounded-lg'>
-              alpha
+            <span className='ms-2 inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10'>
+              Alpha
             </span>
           </div>
           <img
@@ -26,13 +26,13 @@ function App() {
         </div>
         {/* Header Component end */}
         {/* Main Section component */}
-        <main className='mt-12 mb-60'>
+        <main className='mt-8 mb-60'>
           <div className='flex justify-center'>
-            <img src={Gear} alt='Gear' height={150} width={150} />
+            <img src={Gear} alt='Gear' height={140} width={140} />
           </div>
           <div className='mt-10 text-center font-title'>
-            <h1 className='text-6xl font-extrabold'>Documenting Made Easy</h1>
-            <h3 className='mt-4 text-2xl font-medium'>
+            <h1 className='text-5xl font-extrabold'>Documenting Made Easy</h1>
+            <h3 className='mt-4 text-xl font-medium'>
               Upload your project directory and watch the&nbsp;
               <span className='font-extrabold text-sky-600'>
                 AI
@@ -45,11 +45,11 @@ function App() {
           </div>
           {/* file uploader component */}
           <div className='mt-10 flex justify-center'>
-            <label className='font-title flex justify-center items-center me-2 w-1/2 rounded-xl cursor-pointer border-2 border-dashed border-neutral-500 opacity-50'>
+            <label className='font-title flex justify-center items-center me-2 w-2/4 rounded-xl cursor-pointer border-2 border-dashed border-neutral-500 opacity-50'>
               <p className=''>Drag your directory here or click to upload</p>
               <input type='file' hidden />
             </label>
-            <button className='bg-stone-900 border-stone-900 border rounded-xl inline-flex items-center justify-center py-3 px-14 text-center text-base font-medium text-white hover:bg-stone-800 disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5'>
+            <button className='bg-stone-900 border-stone-900 border rounded-xl inline-flex items-center justify-center py-3 px-14 text-center text-sm font-medium text-white hover:bg-stone-800 disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5'>
               Commentify
             </button>
           </div>
